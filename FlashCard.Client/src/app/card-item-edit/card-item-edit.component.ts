78ad08934f29b682f2e93a2e215cf4b9d3e88962
underwater <fares.eidi@gmail.com>
2019-12-10 10:57:34 +0100

    add topic selection in card edit form

--- FlashCard.Client/src/app/card-item-edit/card-item-edit.component.ts
+++ FlashCard.Client/src/app/card-item-edit/card-item-edit.component.ts
@@ -3,6 +3,8 @@ import { Card } from '../models/card.model';
 import { CardsService } from '../services/cards.service';
 import { ActivatedRoute } from '@angular/router';
 import { headersToString } from 'selenium-webdriver/http';
+import { TopicService } from '../services/topic.service';
+import { Topic } from '../models/topic.model';
 
 
 @Component({
@@ -13,9 +15,12 @@ import { headersToString } from 'selenium-webdriver/http';
 export class CardItemEditComponent implements OnInit {
 
   card: Card;
-  constructor(private cardService: CardsService, private activatedRoute: ActivatedRoute) { }
+  topics: Topic[];
+  constructor(private cardService: CardsService, private topicService: TopicService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.topicService.getTopics().subscribe(topics => this.topics = topics);
+
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     console.log(`navigating to id: ${id}`);
 
@@ -23,5 +28,12 @@ export class CardItemEditComponent implements OnInit {
     this.cardService.getCard(Number(id)).subscribe(
       result => this.card = result
     );
+  }
+  save() {
+    console.log('save', this.card);
+
+  }
+  cancel() {
+    console.log('cancel', this.card);
   }
 }
