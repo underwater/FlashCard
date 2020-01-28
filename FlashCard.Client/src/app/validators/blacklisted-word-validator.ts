b0cd69b1c4da1c21a3ee2cf49bb2f789b7565811
OFFICE\Fares <fares.eidi@gmail.com>
2020-01-28 18:33:01 +0100

    add style for validation in form

--- FlashCard.Client/src/app/validators/blacklisted-word-validator.ts
+++ FlashCard.Client/src/app/validators/blacklisted-word-validator.ts
@@ -2,8 +2,9 @@ import { FormControl, AbstractControl } from '@angular/forms';
 
 export function blackListedWordValidator(c: FormControl) {
   const blockedWords = 'xxx';
-  // TODO: why is c null?
-  const isValid = !c.value.inlcudes(blockedWords);
+  const inputData: string = c.value;
+  // TODO: why isn't this working ? https://stackoverflow.com/questions/6629728/check-if-a-string-has-a-certain-piece-of-text
+  const isValid = !inputData.includes(blockedWords);
   return isValid ? null : {
     blackListedWordValidator: {
       valid: false
