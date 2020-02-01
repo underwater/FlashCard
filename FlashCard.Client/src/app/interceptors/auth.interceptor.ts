0b4d0e54a1c9dfcc1862fe02f51cb4a34e07cbba
Dimitris Loukas <dimlucas@outlook.com>
2020-02-01 20:35:38 +0200

    Finish interceptor

--- FlashCard.Client/src/app/interceptors/auth.interceptor.ts
+++ FlashCard.Client/src/app/interceptors/auth.interceptor.ts
@@ -5,15 +5,32 @@ import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+    private _publicRoutes: string[] = [
+        "/api/users/authenticate",
+        "/api/users/signup"
+    ];
+    
+
     constructor(private _authService: AuthService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (req.url.indexOf("/api/users/authenticate") >= 0) {
+        if (this.checkIfPublicRoute(req.url)) {
             return next.handle(req);
         }
         const headers = req.headers
             .set('Authorization', `Bearer ${this._authService.token}`);
         const authReq = req.clone({ headers });
         return next.handle(authReq);
+    }
+
+    private checkIfPublicRoute(url: string): boolean {
+        // for(let route of this._publicRoutes) {
+        //     if (route.indexOf(url) >= 0) {
+        //         return true;
+        //     }
+        // }
+        // return false;
+        //"/api/users/authenticate/api/users/signup"
+        return this._publicRoutes.join("").indexOf(url) >= 0;
     }
 }
