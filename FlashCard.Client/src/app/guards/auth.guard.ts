0d8df31a095f78d53434aa35726fc7a9e0bbb008
Dimitris Loukas <dimlucas@outlook.com>
2020-02-05 20:04:16 +0200

    URL tree in auth.guard

--- FlashCard.Client/src/app/guards/auth.guard.ts
+++ FlashCard.Client/src/app/guards/auth.guard.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router, CanActivateChild } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router, CanActivateChild, UrlTree } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private _authService: AuthService, private _router: Router) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree {
         if (this._authService.isLoggedIn) {
             return true;
         }
         else {
-            this._router.navigate(["signin"]);
-            return false;
+            // this._router.navigate(["signin"]);
+            // return false;
+            return this._router.createUrlTree(["signin"]);
         }
     }
 }
