2c93c15f204872c97348408f9dcee19022ec1c41
OFFICE\Fares <fares.eidi@gmail.com>
2020-02-05 17:53:12 +0100

    add server side logging with serilog

---
+++ FlashCard.Api/Helpers/SerilogConfig.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.MSSqlServer;
+using System.Collections.Generic;
+using Serilog.Events;
+using System.Data;
+
+namespace FlashCard.Api.Helpers
+{
+    public static class SerilogConfig
+    {
+        // https://github.com/serilog/serilog/wiki/Configuration-Basics
+        public static void Configure(IConfiguration configuration)
+        {
+            const string logTable = "Logs";
+            var connectionString = configuration.GetConnectionString("FlashCardDb");
+
+         ColumnOptions columnOptions = new ColumnOptions
+            {
+                AdditionalDataColumns = new List<DataColumn>
+                {
+                    new DataColumn("UserId", typeof(int))
+                },
+            };
+
+            columnOptions.Store.Remove(StandardColumn.Properties);
+            columnOptions.Store.Remove(StandardColumn.MessageTemplate);
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.MSSqlServer(
+                    connectionString: connectionString,
+                    tableName: logTable,
+                    restrictedToMinimumLevel: LogEventLevel.Verbose,
+                    autoCreateSqlTable: true,
+                    columnOptions: columnOptions
+                ).CreateLogger();
+        }
+    }
+}
--- FlashCard.Api/Program.cs
+++ FlashCard.Api/Program.cs
@@ -1,17 +1,31 @@
+using FlashCard.Api.Helpers;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using System;
+using System.IO;
 
 namespace FlashCard.Api
 {
     public class Program
     {
+        // TODO: not sure if the right way to setup logging with serilog ?
+        public static IConfiguration Configuration { get; } = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+           .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+           .Build();
+
         public static void Main(string[] args)
         {
+            SerilogConfig.Configure(Configuration);
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+               .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
