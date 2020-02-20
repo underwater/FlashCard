97673bf57b7a4281bb3efb789b1f078779bd6215
OFFICE\Fares <fares.eidi@gmail.com>
2020-02-20 20:11:00 +0100

    restyled the display of questions

--- FlashCard.Client/src/app/services/questions.service.ts
+++ FlashCard.Client/src/app/services/questions.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Question } from '../models/question.model';
 const httpOptions = {
   headers: new HttpHeaders({ 'content-type': 'application/json' })
 };
+
+// --- q1 --- q2 --- q3
+// ---- q[] ---- q[] ---- q[] --- q[]
 
 @Injectable({ providedIn: 'root' })
 export class QuestionsAdminService {
@@ -12,6 +16,16 @@ export class QuestionsAdminService {
   constructor(private http: HttpClient) { }
 
   getQuestions(): Observable<Question[]> {
-    return this.http.get<Question[]>(this.url, httpOptions);
+    return this.http.get<Question[]>(this.url, httpOptions)
+      .pipe(map(r => r.map(q => this.createQuestion(q))));
+  }
+
+  private createQuestion(question: Question): Question {
+    // @ts-ignore
+    const q = new Question();
+    Object.keys(question).forEach(key => {
+      q[key] = question[key];
+    });
+    return q;
   }
 }
