fe4df92f74f133bf8e34b764a3fc897272061b6f
OFFICE\Fares <fares.eidi@gmail.com>
2020-02-21 12:35:51 +0100

    add emitters for edit / delete in question-item

--- FlashCard.Client/src/app/components/question-item/question-item.component.ts
+++ FlashCard.Client/src/app/components/question-item/question-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Question } from 'src/app/models/question.model';
+
 
 @Component({
   selector: 'app-question-item',
@@ -10,10 +11,19 @@ export class QuestionItemComponent implements OnInit {
 
   constructor() { }
 
+  @Output() public edit: EventEmitter<any> = new EventEmitter();
+  @Output() public delete: EventEmitter<any> = new EventEmitter();
+
   @Input()
   public Question: Question;
 
   ngOnInit() {
   }
 
+  deleteQuestion(question: Question) {
+    this.delete.emit(question);
+  }
+  editQuestion(question: Question) {
+    this.edit.emit(question);
+  }
 }
